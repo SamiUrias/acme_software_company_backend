c4cc38b426cc684bd3468d9d6c40df72ff65987e
Samuel <msamuelurias@gmail.com>
2019-08-08 00:03:19 -0600

    Se puede buscar en Giphy Usando Blade Templates.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,26 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/search', function (Request $request){
+    $endpoint = 'https://api.giphy.com/v1/gifs/search';
+    $giphy_public_key = 'w3jMSakVZZAqtf1M2H3yLLpxoSCURnPy';
+
+    $search_query = $request->query('q');
+
+    if (!$search_query) {
+       return '';
+    }
+    $client = new \GuzzleHttp\Client();
+    $response =  $client->request('GET', $endpoint, [
+        'query' => [
+            'api_key' => $giphy_public_key,
+            'q' => $search_query
+        ]
+    ]);
+
+    $statusCode = $response->getStatusCode();
+    $content = $response->getBody();
+    return $content;
+    return 'Hello World';
+});
